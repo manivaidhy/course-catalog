505f6a384c6beeb9642f115f0ae62f80be8b0d58
Manikandan WSL <manibboyz@gmail.com>
2020-07-27 19:04:45 +0530

    Production build with minified files

---
+++ build/precache-manifest.0dc306b9b95c660f12d4d45a38ef9561.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2167eb6db606bd66eea1",
+    "url": "/static/js/main.2a3f5ccf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b30db925a09b56fdf0b4",
+    "url": "/static/js/2.db039fac.chunk.js"
+  },
+  {
+    "revision": "8b3b26276241d503f0612794b66319ff",
+    "url": "/index.html"
+  }
+];
