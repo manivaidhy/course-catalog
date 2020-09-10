380b55ea8230a22d20aa425ff396393e93732e90
Manikandan Vaidhyanathan <ashesh.code@gmail.com>
2020-09-11 02:56:45 +0530

    Updates

---
+++ precache-manifest.7753fd5d8abe5e7a29592087503a022b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d12f56ac6d39aadbbb6",
+    "url": "/course-catalog/static/js/main.2e630ff3.chunk.js"
+  },
+  {
+    "revision": "09a150546fcb5c891012",
+    "url": "/course-catalog/static/js/runtime~main.47df68de.js"
+  },
+  {
+    "revision": "b30db925a09b56fdf0b4",
+    "url": "/course-catalog/static/js/2.db039fac.chunk.js"
+  },
+  {
+    "revision": "526b47a02790768556ff5ef1f99db430",
+    "url": "/course-catalog/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/course-catalog/precache-manifest.7753fd5d8abe5e7a29592087503a022b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/course-catalog/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
